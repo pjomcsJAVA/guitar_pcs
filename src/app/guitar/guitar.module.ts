bed5676bbebdab62d87e552318be3e476f416e50
Pedro Costa e Silva <pjosilva@criticalsoftware.com>
2019-06-05 14:32:27 +0100

    FIrst commit

---
+++ src/app/guitar/guitar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GuitarRoutingModule } from './guitar-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GuitarRoutingModule
+  ]
+})
+export class GuitarModule { }
